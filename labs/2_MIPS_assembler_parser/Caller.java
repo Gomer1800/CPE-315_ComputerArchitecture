d5905df90d8372910068e63dfd4ecd5970f178d7
Luis Gomez <luisgomez1800@gmail.com>
2019-10-27 23:10:26 -0700

    starting to work remotely with yu

--- labs/2_MIPS_assembler_parser/Caller.java
+++ labs/2_MIPS_assembler_parser/Caller.java
@@ -2,21 +2,20 @@ import java.lang.*;
 import java.util.List;
 
 public class Caller{
-   public List<String> functionCaller(boolean isScript, List<List<String>> tokenized) {
+   public List<String> fnCallStdin() {
       List<String> nextCommand;
-      if (isScript) {
-         nextCommand = tokenized.get(0);
-         tokenized.remove(0);   // remove, so that you don't have to keep track of indices
-         return nextCommand;
-      }
-      else {  //stdin
-         List<String> lineList = readCommands.parseCommands(false, "");
-         Parser parse = new Parser();
-         parse.tokenize(lineList);
-         tokenized = parse.gettokenized();
-         nextCommand = tokenized.get(0);
-         tokenized.remove(0);
-         return nextCommand;
-      }
+      List<String> lineList = readCommands.parseCommands(false, "");
+      Parser parse = new Parser();
+      parse.tokenize(lineList);
+      List<List<String>> tokenized = parse.gettokenized();
+      nextCommand = tokenized.get(0);
+      tokenized.remove(0);
+      return nextCommand;
+   }
+  
+   public List<String> fnCallScript(List<List<String>> tokenized) {
+      List<String> nextCommand = tokenized.get(0);
+      tokenized.remove(0);   // remove, so that you don't have to keep track of indices
+      return nextCommand;
    }
 }
--- labs/2_MIPS_assembler_parser/Parser.java
+++ labs/2_MIPS_assembler_parser/Parser.java
@@ -6,13 +6,16 @@ import java.io.*;
 import java.lang.*;
 
 public class Parser {
+
+   public boolean _isScript;
+   
    private List<List<String>> _tokenized = new ArrayList<List<String>>();
    
    public List<List<String>> gettokenized() {
       return _tokenized;
    }
 
-   public void tokenize(List<String> CommandList) {
+   public List<List<String>> tokenize(List<String> CommandList) {
       List<String> tokens;
       
       for (int i=0; i< CommandList.size(); i++) { 
@@ -24,9 +27,11 @@ public class Parser {
             _tokenized.add(tokens);
          }
       }
+      return _tokenized;
    }
     
-   private boolean is_blank( List<String> tokens) {
+   private boolean is_blank( List<String> tokens)
+   {
       if (tokens.isEmpty())   { 
          return true; 
       }
@@ -35,6 +40,35 @@ public class Parser {
       }      
       else {
          return false;
+      }
+   }
+   
+   public void fromStdin(List<String> cmd)
+   {
+      System.out.println("fromStdin()");
+      List<String> nextCommand;
+      List<String> lineList = readCommands.parseCommands(false, "");
+      this.tokenize(lineList);
+      List<List<String>> tokenized = this.gettokenized();
+      cmd = tokenized.get(0);
+      tokenized.remove(0);
+   }
+
+   public void fromScript(List<String> cmd)
+   {
+      System.out.println("fromScript()");
+      cmd = _tokenized.get(0);
+      _tokenized.remove(0);   // remove, so that you don't have to keep track of indices
+   }
+   
+   public void print() 
+   {
+      /* print tokenized List */
+      for (int i=0; i < _tokenized.size(); i++) {
+         for (int j = 0; j < _tokenized.get(i).size(); j++) {
+            System.out.print(_tokenized.get(i).get(j) + " ");
+         }
+         System.out.println();
       }
    }
 }     
