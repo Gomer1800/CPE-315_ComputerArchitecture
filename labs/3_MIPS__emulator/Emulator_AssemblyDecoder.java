c395fb4eaca5e9fe042353893797402ae002b6d4
Luis Gomez <luisgomez1800@gmail.com>
2019-11-11 17:32:06 -0800

    fully integrated Yus working code for emulator assembly decoder

--- labs/3_MIPS__emulator/Emulator_AssemblyDecoder.java
+++ labs/3_MIPS__emulator/Emulator_AssemblyDecoder.java
@@ -7,58 +7,143 @@ public class Emulator_AssemblyDecoder {
    public int decodeAssembly(/* Line of Mips Code, Register Memory */
       List<String> assemblyCode,
       int [] regMem,
-      int PC) 
+      int PC,
+      int [] dataMem,
+      int SP)
    {
       String inst = assemblyCode.get(0);
 
       if (inst.equals("addi")) {
       // rt = rs + immed
-         System.out.println("addi");
-         int rs    = regMem[Helpers._RegNum(assemblyCode.get(2))];
+         //System.out.println("addi " + assemblyCode.get(2) + " = " + assemblyCode.get(1) + " + " + assemblyCode.get(3));
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(2))];
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(1))];
          int immed = Integer.parseInt(assemblyCode.get(3));
-         regMem[Helpers._RegNum(assemblyCode.get(1))] = rs + immed;
+         regMem[Helpers._RegNum(assemblyCode.get(2))] = rs + immed;
          return (PC+1);
       }
       else if (inst.equals("beq")) {
       // offset if rs == rt 
-         String rs =    Helpers._5bit_register_decoder(assemblyCode.get(1));
-         String rt =    Helpers._5bit_register_decoder(assemblyCode.get(2));
-         String immed = Helpers._16bit_signed(Integer.parseInt(assemblyCode.get(3)));
-         //System.out.println(code + " " + rs + " " + rt + " " + immed);         
+       //  System.out.println("beq " + assemblyCode.get(1) + " == " + assemblyCode.get(2) + ", relative offset = " + assemblyCode.get(3));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(1))];
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(2))];
+         int immed = Integer.parseInt(assemblyCode.get(3));
+         if (rs == rt) {
+            return (PC + 1 + immed);
+         }
+         return (PC+1);
       }
-      else if ((inst.equals("addi")) | (inst.equals("beq")) | (inst.equals("bne"))) {
-         String rs =    Helpers._5bit_register_decoder(assemblyCode.get(1));
-         String rt =    Helpers._5bit_register_decoder(assemblyCode.get(2));
-         String immed = Helpers._16bit_signed(Integer.parseInt(assemblyCode.get(3)));
-         //System.out.println(code + " " + rs + " " + rt + " " + immed);         
+      // offset if rs != rt 
+      else if (inst.equals("bne")) {
+       //  System.out.println("bne " + assemblyCode.get(1) + " != " + assemblyCode.get(2));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(1))];
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(2))];
+         int immed = Integer.parseInt(assemblyCode.get(3));
+         if (rs != rt){
+            return (PC + 1 + immed);
+         }
+         return (PC+1);
       }
-      else if ((inst.equals("add")) | (inst.equals("sub")) |  
-         (inst.equals("slt")) | (inst.equals("and")) | (inst.equals("or"))) {
-         String rd = Helpers._5bit_register_decoder(assemblyCode.get(1));
-         String rs = Helpers._5bit_register_decoder(assemblyCode.get(2));
-         String rt = Helpers._5bit_register_decoder(assemblyCode.get(3));
-         //System.out.println("000000 " +  rs + " " + rt + " " + rd + " 00000 " + code);
+      //  rd = rs + rt
+      else if (inst.equals("add")) {
+       //  System.out.println("add " + assemblyCode.get(2) + " + " + assemblyCode.get(3));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(2))];
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(3))];
+         regMem[Helpers._RegNum(assemblyCode.get(1))] = rs + rt;
+         return (PC+1);
       }
-      else if ((inst.equals("j")) | (inst.equals("jal"))) {
-         String immed = Helpers._26bit_signed(Integer.parseInt(assemblyCode.get(1)));
-         //System.out.println(code + " " + immed);
+      // rd = rs - rt
+      else if (inst.equals("sub")) {
+       //  System.out.println("sub " + assemblyCode.get(2) + " - " + assemblyCode.get(3));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(2))];
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(3))];
+         regMem[Helpers._RegNum(assemblyCode.get(1))] = rs - rt;
+         return (PC+1);
       }
-      else if ((inst.equals("jr"))) {
-         String rs = Helpers._5bit_register_decoder(assemblyCode.get(1));
-         //System.out.println("000000 " + rs + " 000000000000000 " + code);
+      // rd = rs & rt 
+       else if (inst.equals("and")) {
+       //  System.out.println("and " + assemblyCode.get(2) + " & " + assemblyCode.get(3));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(2))];
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(3))];
+         regMem[Helpers._RegNum(assemblyCode.get(1))] = rs & rt;
+         return (PC+1);
       }
+      // rd = rs | rt 
+      else if (inst.equals("or")) {
+       //  System.out.println("or " + assemblyCode.get(2) + " | " + assemblyCode.get(3));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(2))];
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(3))];
+         regMem[Helpers._RegNum(assemblyCode.get(1))] = rs | rt;
+         return (PC+1);
+      }
+      // set rd = 1 if rs < rt else rd = 0
+      else if (inst.equals("slt")) {
+       //  System.out.println("slt " + assemblyCode.get(2) + " < " + assemblyCode.get(3));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(2))];
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(3))];
+         if (rs < rt) {
+            regMem[Helpers._RegNum(assemblyCode.get(1))] = 1;
+         }
+         else{
+            regMem[Helpers._RegNum(assemblyCode.get(1))] = 0; 
+         }
+         return (PC+1);
+      }
+      // offset by immed
+      else if(inst.equals("j")) {   // not sure if it's correct
+       //  System.out.println("j " + assemblyCode.get(1));
+         int immed = Integer.parseInt(assemblyCode.get(1)); 
+         return (immed); 
+      }
+      // offet by rs
+      else if(inst.equals("jr")) {
+       //  System.out.println("jr " + assemblyCode.get(1));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(1))];
+         return rs;  //needs PC+1?  
+      }    
+      // offset by immed and link ra 
+      else if(inst.equals("jal")) {
+         //System.out.println("jal " + assemblyCode.get(1));
+         int immed = Integer.parseInt(assemblyCode.get(1));
+         regMem[31] = PC + 1;  //regMem[31] = $ra
+         return immed;
+      }
+      // rs = rt << shamt
+      else if(inst.equals("sll")) {
+       //  System.out.println("sll " + assemblyCode.get(2) + " << " + assemblyCode.get(3));
+         int rt   = regMem[Helpers._RegNum(assemblyCode.get(2))];     
+         int shamt = Integer.parseInt(assemblyCode.get(3));
+         regMem[Helpers._RegNum(assemblyCode.get(1))] = rt << shamt;
+         return (PC+1);
+      }
+      
+      else if (inst.equals("lw")){
+         //System.out.println("lw " + assemblyCode.get(1) + " = Mem[ " + assemblyCode.get(2) + " + " + assemblyCode.get(3) + "]");
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(1))];
+         int immed = Integer.parseInt(assemblyCode.get(2));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(3))]; 
+         rt = regMem[rs + immed]; 
+         return (PC+1);
+      }
+      
+      else if (inst.equals("sw")){
+        // System.out.println("sw " + "Mem[ " + assemblyCode.get(2) + " + " + assemblyCode.get(3) + "] = " + assemblyCode.get(1)); 
+         int rt    = regMem[Helpers._RegNum(assemblyCode.get(1))];
+         int immed = Integer.parseInt(assemblyCode.get(2));
+         int rs    = regMem[Helpers._RegNum(assemblyCode.get(3))]; 
+         rt = regMem[rs + immed]; 
+         return (PC+1);
+      }
+      
+
+      /*
       else if ((inst.equals("lw")) | (inst.equals("sw"))) {
          String rt =    Helpers._5bit_register_decoder(assemblyCode.get(1));  
          String immed = Helpers._16bit_signed(Integer.parseInt(assemblyCode.get(2)));
          String rs =    Helpers._5bit_register_decoder(assemblyCode.get(3));
          //System.out.println(code + " " + rs + " " + rt + " " + immed); 
       }
-      else if (inst.equals("sll")) {
-         String rd =    Helpers._5bit_register_decoder(assemblyCode.get(1));
-         String rt =    Helpers._5bit_register_decoder(assemblyCode.get(2));
-         String shamt = Helpers._5bit_unsigned(Integer.parseInt(assemblyCode.get(3)));
-         //System.out.println("000000 00000 " + rt + " " + rd + " " + shamt + " " + code); 
-      } 
+      */
       return PC;
    }
 }   
